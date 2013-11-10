4875ff1bfaf86d5ea2d8615d895b71a77c9fa24c
Dan Cornwell <dtcornwell@live.com>
2013-11-10 10:47:28 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DTCInsuranceBroker::Application.config.secret_key_base = 'c42471380c9d802a80f196ac83cef43ae435487572b20158e5807ee7dc1c0945634425e7df2f34599220d0061711b9da20e9242c3bf57f2f28a68678149820fe'
