85577fbe267ace12f52df4616487ebcc75647569
Dan Cornwell <dtcornwell@live.com>
2013-11-13 19:07:13 +0000

    Moved cancel application button, some more tests

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,47 +4,69 @@ describe "Pages" do
 
   subject {page}
 
+  shared_examples_for "all pages" do
+
+    it {should have_title(full_title(page_title))}
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_title(full_title('About'))
+      click_link "Help"
+      expect(page).to have_title(full_title('Help'))
+      click_link "Home"
+      expect(page).to have_title(full_title(''))
+    end
+  end
+
   describe "Index Page" do
     before { visit root_path }
 
     it {should have_content('Welcome to DTC online insurance broker!')}
-    it {should have_title(full_title(''))}
+    let(:page_title) {''}
+    it_should_behave_like "all pages"
     it {should_not have_title('| Home')}
+
   end
 
   describe "User Form Page" do
     before { visit user_form_path }
 
     it {should have_content('Please fill in the forms below:')}
-    it {should have_title(full_title('User Form'))}
+    let(:page_title) {'User Form'}
+    it_should_behave_like "all pages"
+    it {should have_selector('form')}
   end
 
   describe "About Page" do
     before { visit about_path }
 
-    it {should have_title(full_title('About'))}
+    let(:page_title) {'About'}
+    it_should_behave_like "all pages"
 
   end
 
   describe "Help Page" do
     before { visit help_path }
 
-    it {should have_title(full_title('Help'))}
+    let(:page_title) {'Help'}
+    it_should_behave_like "all pages"
 
   end
 
   describe "Error Page" do
     before {visit error_path}
 
+    let(:page_title) {'Error'}
     it {should have_content('An error occurred!')}
-    it {should have_title(full_title('Error'))}
+    it_should_behave_like "all pages"
 
   end
 
   describe "Quote Page" do
     before {visit quote_path}
 
-    it {should have_title(full_title('Your Quote'))}
+    let(:page_title) {'Your Quote'}
+    it_should_behave_like "all pages"
 
   end
 
